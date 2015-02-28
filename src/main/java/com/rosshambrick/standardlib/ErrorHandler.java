2426f96b5a0fb89bd7f1d34253cf8ceec198c7bf
Ross Hambrick <ross.hambrick@stablekernel.com>
2015-02-28 15:32:56 -0500

    "Added a handleError() overload"

--- src/main/java/com/rosshambrick/standardlib/ErrorHandler.java
+++ src/main/java/com/rosshambrick/standardlib/ErrorHandler.java
@@ -5,6 +5,10 @@ import android.util.Log;
 import android.widget.Toast;
 
 public class ErrorHandler {
+    public static void handleError(Context context, Throwable e) {
+        ErrorHandler.handleError(context, e.getLocalizedMessage(), e);
+    }
+
     public static void handleError(Context context, String message, Throwable e) {
         Log.e(context.getClass().getSimpleName(), e.getLocalizedMessage(), e);
         if (BuildConfig.DEBUG || message != null) {
