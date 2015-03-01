8101e11b1d20a397c0fcbfa925b73f0cff433e44
Ross Hambrick <ross.hambrick@stablekernel.com>
2015-02-28 22:42:14 -0500

    Added a setter for debug mode and added some javadoc comments

--- src/main/java/com/rosshambrick/standardlib/Log.java
+++ src/main/java/com/rosshambrick/standardlib/Log.java
@@ -1,8 +1,27 @@
 package com.rosshambrick.standardlib;
 
+/**
+ * This Log class is a proxy to the standard android.util.Log except
+ * that it will disable debug logging for release builds.
+ */
 public class Log {
+    private static boolean DEBUG = true;
+
+    /**
+     * Since gradle does not support passing the BuildConfig.DEBUG value
+     * down to library modules, you will need to call this this method in
+     * your Application.onCreate() method like so:
+     *
+     *      Log.setDebug(BuildConfig.DEBUG);
+     *
+     * @param isDebug
+     */
+    public static void setDebug(boolean isDebug) {
+        DEBUG = isDebug;
+    }
+
     public static void d(String tag, String msg) {
-        if (BuildConfig.DEBUG) android.util.Log.d(tag, msg);
+        if (DEBUG) android.util.Log.d(tag, msg);
     }
 
     public static void e(String tag, String msg, Throwable e) {
