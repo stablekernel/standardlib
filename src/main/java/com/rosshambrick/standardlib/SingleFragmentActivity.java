f554ea32fae8c6f4d759b8bcce04cc8b71f32938
Ross Hambrick <ross.hambrick@stablekernel.com>
2015-03-03 23:28:53 -0500

    Added java docs to the SingleFragmentActivity

--- src/main/java/com/rosshambrick/standardlib/SingleFragmentActivity.java
+++ src/main/java/com/rosshambrick/standardlib/SingleFragmentActivity.java
@@ -13,12 +13,46 @@ public class SingleFragmentActivity extends ActionBarActivity {
     private Toolbar toolbar;
     private boolean traceLog;
 
-    public static <TFragment extends Fragment> Intent newIntent(Context context, Class<TFragment> fragmentName, Bundle fragmentArgs) {
-        return SingleFragmentActivity.newIntent(context, fragmentName, fragmentArgs, SingleFragmentActivity.class);
+    /**
+     * This allows you to create an Intent that will start a new SingleFragmentActivity that
+     * automatically instantiates the Fragment class specified and adds it to the fragment container
+     *
+     * @param context
+     * @param fragmentClass
+     * @return Intent
+     */
+    public static Intent newIntent(Context context, Class<? extends Fragment> fragmentClass) {
+        return newIntent(context, fragmentClass, null);
     }
 
-    public static <TFragment extends Fragment, TActivity extends SingleFragmentActivity> Intent newIntent(Context context, Class<TFragment> fragmentName, Bundle fragmentArgs, Class<TActivity> activityClass) {
-        Intent intent = new Intent(context, activityClass);
+    /**
+     * This allows you to create an Intent that will start a new SingleFragmentActivity that
+     * automatically instantiates the Fragment class specified and adds it to the fragment container.
+     *
+     * This method is useful but somewhat misleading since your Fragment instance is only used to
+     * get the Class and Fragment Arguments to pass along to the SingleFragmentActivity
+     *
+     * @param context
+     * @param fragment
+     * @return
+     */
+    public static Intent newIntent(Context context, Fragment fragment) {
+        return newIntent(context, fragment.getClass(), fragment.getArguments());
+    }
+
+    /**
+     * This allows you to create an Intent that will start a new SingleFragmentActivity that
+     * automatically instantiates the Fragment class specified, adds it to the fragment container,
+     * and passes along the Fragment Arguments.
+     *
+     * @param context
+     * @param fragmentName
+     * @param fragmentArgs
+     * @param <TFragment>
+     * @return
+     */
+    public static <TFragment extends Fragment> Intent newIntent(Context context, Class<TFragment> fragmentName, Bundle fragmentArgs) {
+        Intent intent = new Intent(context, SingleFragmentActivity.class);
         Bundle extras;
         if (fragmentArgs != null) {
             extras = new Bundle(fragmentArgs);
