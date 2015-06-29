022134c718c56a53c4e4315c9bd9c5d0be1c7b73
Bryan Richardson <bryan.richardson@stablekernel.com>
2015-06-29 13:45:53 -0400

    Removed setTitle

--- src/main/java/com/stablekernel/standardlib/OkCancelFragment.java
+++ src/main/java/com/stablekernel/standardlib/OkCancelFragment.java
@@ -82,7 +82,6 @@ public class OkCancelFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity())
-                .setTitle(title)
                 .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
