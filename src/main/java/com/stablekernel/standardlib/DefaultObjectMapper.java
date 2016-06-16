4b09068f2483e78c9336792fca70fc303d21f913
BryanJonesRichardson <bryanjonesrichardson@gmail.com>
2016-06-16 15:36:56 -0400

    Added deserializtion for date/time

--- src/main/java/com/stablekernel/standardlib/DefaultObjectMapper.java
+++ src/main/java/com/stablekernel/standardlib/DefaultObjectMapper.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.text.SimpleDateFormat;
+import java.util.TimeZone;
 
 public class DefaultObjectMapper extends ObjectMapper {
 
@@ -14,5 +15,6 @@ public class DefaultObjectMapper extends ObjectMapper {
         setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE);
         setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.DEFAULT);
         setDateFormat(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss"));
+        setTimeZone(TimeZone.getTimeZone("UTC"));
     }
 }
