7ec555d39168ae5468bcfd1259de0587e0fb2c8e
BryanJonesRichardson <bryanjonesrichardson@gmail.com>
2016-06-21 16:46:24 -0400

    Added check for null traget fragment

--- src/main/java/com/stablekernel/standardlib/OkCancelFragment.java
+++ src/main/java/com/stablekernel/standardlib/OkCancelFragment.java
@@ -87,13 +87,17 @@ public class OkCancelFragment extends DialogFragment {
                     public void onClick(DialogInterface dialog, int which) {
                         Intent data = new Intent();
                         data.putExtra(DATA_ENTITY_ID, entityId);
-                        getTargetFragment().onActivityResult(getTargetRequestCode(), Activity.RESULT_OK, data);
+                        if (getTargetFragment() != null) {
+                            getTargetFragment().onActivityResult(getTargetRequestCode(), Activity.RESULT_OK, data);
+                        }
                     }
                 })
                 .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        getTargetFragment().onActivityResult(getTargetRequestCode(), Activity.RESULT_CANCELED, null);
+                        if (getTargetFragment() != null) {
+                            getTargetFragment().onActivityResult(getTargetRequestCode(), Activity.RESULT_CANCELED, null);
+                        }
                     }
                 });
 
