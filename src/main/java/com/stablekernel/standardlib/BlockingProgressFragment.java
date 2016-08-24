aefa64ff6d4eefd43b7d6c3e042c20e63b5d7163
BryanJonesRichardson <bryanjonesrichardson@gmail.com>
2016-08-24 10:07:05 -0400

    Added capability for blockingProgress to have text

--- src/main/java/com/stablekernel/standardlib/BlockingProgressFragment.java
+++ src/main/java/com/stablekernel/standardlib/BlockingProgressFragment.java
@@ -11,15 +11,35 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.TextView;
 
 import rx.Subscription;
 
 public class BlockingProgressFragment extends DialogFragment {
     public static final String TAG = BlockingProgressFragment.class.getSimpleName();
+    private static final String ARGS_PROGRESS_TEXT = "ARGS_PROGRESS_TEXT";
     private Subscription subscription;
+    private String progressText;
 
     public static BlockingProgressFragment newInstance() {
         return new BlockingProgressFragment();
+    }
+
+    public static BlockingProgressFragment newInstance(String progressText) {
+        BlockingProgressFragment fragment = new BlockingProgressFragment();
+        Bundle args = new Bundle();
+        args.putString(ARGS_PROGRESS_TEXT, progressText);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            progressText = getArguments().getString(ARGS_PROGRESS_TEXT);
+        }
     }
 
     @NonNull
@@ -34,6 +54,16 @@ public class BlockingProgressFragment extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_blocking_progress, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        if (progressText != null) {
+            TextView progressTextView = (TextView) view.findViewById(R.id.progress_text);
+            progressTextView.setText(progressText);
+        }
     }
 
     @Override
